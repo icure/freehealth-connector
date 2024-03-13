9b09a79f10fc033a54159629ba5e4b92a2fecfb2
Maxime Mennechet <maxime.mennechet@gmail.com>
2024-03-13 14:49:06 +0100

    Fixed STSServiceImpl.kt

--- src/main/kotlin/org/taktik/freehealth/middleware/service/impl/STSServiceImpl.kt
+++ src/main/kotlin/org/taktik/freehealth/middleware/service/impl/STSServiceImpl.kt
@@ -420,6 +420,32 @@ class STSServiceImpl(val keystoresMap: IMap<UUID, ByteArray>, val tokensMap: IMa
                     "urn:be:fgov:certified-namespace:ehealth"
                 )
             )
+            "reeducation" -> listOf(
+                SAMLAttributeDesignator(
+                    "urn:be:fgov:ehealth:1.0:reeducation:nihii-number",
+                    "urn:be:fgov:identification-namespace"
+                ),
+                SAMLAttributeDesignator(
+                    "urn:be:fgov:ehealth:1.0:certificateholder:reeducation:nihii-number",
+                    "urn:be:fgov:identification-namespace"
+                ),
+                SAMLAttributeDesignator(
+                    "urn:be:fgov:ehealth:1.0:reeducation:nihii-number:recognisedreeducation:boolean",
+                    "urn:be:fgov:certified-namespace:ehealth"
+                ),
+                SAMLAttributeDesignator(
+                    "urn:be:fgov:ehealth:1.0:certificateholder:reeducation:nihii-number:recognisedreeducation:boolean",
+                    "urn:be:fgov:certifiednamespace:ehealth"
+                ),
+                SAMLAttributeDesignator(
+                    "urn:be:fgov:ehealth:1.0:reeducation:nihii-number:recognisedreeducation:nihii11",
+                    "urn:be:fgov:certifiednamespace:ehealth"
+                ),
+                SAMLAttributeDesignator(
+                    "urn:be:fgov:ehealth:1.0:certificateholder:recognisedorganization:boolean",
+                    "urn:be:fgov:certified-namespace:ehealth"
+                )
+            )
             else -> throw IllegalArgumentException("unsupported quality")
         } + extraDesignators.map { SAMLAttributeDesignator(it.second, it.first) }
 
