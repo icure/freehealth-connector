859cdadcff21191dc000563c0685b9b7a9f8a087
Adrian <>
2024-09-16 23:07:31 +0300

    update MDA to support mcn licenses specific to hcpQuality

--- src/main/java/org/taktik/connector/business/mycarenetdomaincommons/util/McnConfigUtil.java
+++ src/main/java/org/taktik/connector/business/mycarenetdomaincommons/util/McnConfigUtil.java
@@ -4,12 +4,18 @@ import org.taktik.connector.business.mycarenetdomaincommons.domain.McnPackageInf
 import org.taktik.connector.technical.config.ConfigFactory;
 import org.taktik.connector.technical.config.ConfigValidator;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.taktik.connector.technical.exception.CredentialException;
+
 public final class McnConfigUtil {
    private static final String PACKAGE_LICENSE_USERNAME = "package.license.username";
    private static final String PACKAGE_LICENSE_PASSWORD = "package.license.password";
    private static final String PACKAGE_LICENSE_NAME = "package.name";
 
    private static final ConfigValidator configValidator = ConfigFactory.getConfigValidator();
+
+   private static final Logger LOG = LoggerFactory.getLogger(McnConfigUtil.class);
 
    private McnConfigUtil() {
       throw new UnsupportedOperationException();
@@ -20,5 +26,27 @@ public final class McnConfigUtil {
       String password = licensePassword != null ? licensePassword : configValidator.getProperty(componentName + "." + PACKAGE_LICENSE_PASSWORD, "${mycarenet.license.password}");
       String name = packageName != null ? packageName : configValidator.getProperty(componentName + "." + PACKAGE_LICENSE_NAME, "${package.name}");
       return new McnPackageInfo(userName.trim(), password.trim(), name);
+   }
+
+   public static McnPackageInfo retrievePackageInfo(String componentName, String packageName, String hcpQuality) {
+      String userNameKey = "mycarenet.license." + hcpQuality + ".username";
+      String passwordKey = "mycarenet.license." + hcpQuality + ".password";
+
+      LOG.info("retrievePackageInfo retrieving info using keys: " + userNameKey + " " + passwordKey);
+
+      String userName = hcpQuality != null ? configValidator.getProperty(userNameKey) : null;
+      String password = hcpQuality != null ? configValidator.getProperty(passwordKey) : null;
+
+      if(userName == null){
+         LOG.error("MCN license username for key "+ userNameKey + " is null");
+         throw new CredentialException(new Exception("MCN license information invalid"));
+      }
+      if(password == null){
+         LOG.error("MCN license password for key "+ passwordKey + " is null");
+         throw new CredentialException(new Exception("MCN license information invalid"));
+      }
+
+
+      return new McnPackageInfo(userName.trim(), password.trim(), "rosa");
    }
 }
