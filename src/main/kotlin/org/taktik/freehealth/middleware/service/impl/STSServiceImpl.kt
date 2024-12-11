e676bc087c1169a2afd14ccb30ea3e0859c4c9f0
Maxime Mennechet <maxime.mennechet@gmail.com>
2024-12-11 21:37:22 +0100

    Added possibility to create token with ehp ceritficate

--- src/main/kotlin/org/taktik/freehealth/middleware/service/impl/STSServiceImpl.kt
+++ src/main/kotlin/org/taktik/freehealth/middleware/service/impl/STSServiceImpl.kt
@@ -428,6 +428,10 @@ class STSServiceImpl(val keystoresMap: IMap<UUID, ByteArray>, val tokensMap: IMa
                 SAMLAttributeDesignator(
                     "urn:be:fgov:ehealth:1.0:certificateholder:recognisedorganization:boolean",
                     "urn:be:fgov:certified-namespace:ehealth"
+                ),
+                SAMLAttributeDesignator(
+                    "urn:be:fgov:kbo-bce:organization:cbe-number",
+                    "urn:be:fgov:identification-namespace"
                 )
             )
             "ehp" -> listOf(
