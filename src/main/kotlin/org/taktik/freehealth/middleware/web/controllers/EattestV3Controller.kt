11e4415e93d78431734d461539cfe1eda55bee9d
Maxime <38285320+maxime2018@users.noreply.github.com>
2025-01-30 10:23:35 +0100

    Update src/main/kotlin/org/taktik/freehealth/middleware/web/controllers/EattestV3Controller.kt
    Co-authored-by: Antoine Duchâteau <ad@icure.com>

--- src/main/kotlin/org/taktik/freehealth/middleware/web/controllers/EattestV3Controller.kt
+++ src/main/kotlin/org/taktik/freehealth/middleware/web/controllers/EattestV3Controller.kt
@@ -127,7 +127,7 @@ class EattestV3Controller(val eattestService: EattestV3Service) {
         hcpFirstName,
         hcpLastName,
         hcpCbe,
-        hcpQuality,
+        hcpQuality ?: "persphysician",
         treatmentReason,
         traineeSupervisorSsin,
         traineeSupervisorNihii,
