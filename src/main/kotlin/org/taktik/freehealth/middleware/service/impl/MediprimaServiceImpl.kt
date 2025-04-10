4fa48f96fe353d00ecd356ab7057d839d37579b8
Maxime Mennechet <maxime.mennechet@gmail.com>
2025-04-10 16:19:46 +0200

    Wip on Medisprima test

--- src/main/kotlin/org/taktik/freehealth/middleware/service/impl/MediprimaServiceImpl.kt
+++ src/main/kotlin/org/taktik/freehealth/middleware/service/impl/MediprimaServiceImpl.kt
@@ -1,27 +1,21 @@
 package org.taktik.freehealth.middleware.service.impl
 
-import be.fgov.ehealth.etee.crypto.utils.KeyManager
 import be.fgov.ehealth.mediprima.protocol.v2.BySsinType
 import be.fgov.ehealth.mediprima.protocol.v2.ConsultCarmedDataType
 import be.fgov.ehealth.mediprima.protocol.v2.ConsultCarmedInterventionResponse
-import com.sun.org.apache.xerces.internal.jaxp.datatype.XMLGregorianCalendarImpl
 import ma.glasnost.orika.MapperFacade
-import org.joda.time.DateTime
 import org.slf4j.LoggerFactory
-import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.stereotype.Service
 import org.taktik.connector.business.mediprimav2.service.impl.MediprimaServiceImpl
-import org.taktik.connector.technical.service.etee.CryptoFactory
 import org.taktik.connector.technical.service.keydepot.KeyDepotService
 import org.taktik.connector.technical.service.keydepot.impl.KeyDepotManagerImpl
-import org.taktik.connector.technical.service.sts.security.impl.KeyStoreCredential
-import org.taktik.freehealth.middleware.dao.User
 import org.taktik.freehealth.middleware.exception.MissingTokenException
 import org.taktik.freehealth.middleware.service.MediprimaService
 import org.taktik.freehealth.middleware.service.STSService
 import java.time.Instant
 import java.util.*
 import javax.xml.datatype.DatatypeFactory
+import javax.xml.datatype.XMLGregorianCalendar
 
 @Service
 class MediprimaServiceImpl(val stsService: STSService, keyDepotService: KeyDepotService, val mapper: MapperFacade) : MediprimaService {
@@ -45,14 +39,18 @@ class MediprimaServiceImpl(val stsService: STSService, keyDepotService: KeyDepot
     ): ConsultCarmedInterventionResponse? {
         val samlToken =
             stsService.getSAMLToken(tokenId, keystoreId, passPhrase)
-                ?: throw MissingTokenException("Cannot obtain token for Genins operations")
-
+                ?: throw MissingTokenException("Cannot obtain token for Mediprima operations")
+        val datatypeFactory: DatatypeFactory = DatatypeFactory.newInstance()
+        val now: GregorianCalendar = GregorianCalendar(TimeZone.getTimeZone("Europe/Brussels")).apply {
+            time = Date()
+        }
+        val issueInstant: XMLGregorianCalendar = datatypeFactory.newXMLGregorianCalendar(now)
         val consultCaremedRequest = be.fgov.ehealth.mediprima.protocol.v2.ConsultCarmedInterventionRequestType().apply {
-            issueInstant = XMLGregorianCalendarImpl(DateTime.now().toGregorianCalendar())
-            selectionCriteria = ConsultCarmedDataType().apply {
-                bySsin = BySsinType().apply {
+            this.issueInstant = issueInstant
+            this.selectionCriteria = ConsultCarmedDataType().apply {
+                this.bySsin = BySsinType().apply {
                     this.ssin = patientSsin
-                    this.referenceDate = DatatypeFactory.newInstance().newXMLGregorianCalendar(referenceDate.toString())
+                    this.referenceDate = issueInstant//DatatypeFactory.newInstance().newXMLGregorianCalendar(referenceDate.toString())
                 }
             }
         }
