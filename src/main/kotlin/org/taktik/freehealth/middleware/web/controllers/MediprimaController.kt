847d9c552e3e211d3deae6f21422ee5e53f74e12
Mennechet <maxime.mennechet@gmail.com>
2025-06-18 16:58:38 +0200

    Wip on Mediprima tarification service

--- src/main/kotlin/org/taktik/freehealth/middleware/web/controllers/MediprimaController.kt
+++ src/main/kotlin/org/taktik/freehealth/middleware/web/controllers/MediprimaController.kt
@@ -1,17 +1,22 @@
 package org.taktik.freehealth.middleware.web.controllers
 
-import be.fgov.ehealth.mediprima.protocol.v2.ConsultCarmedInterventionResponse
 import be.fgov.ehealth.mediprima.protocol.v2.ConsultCarmedInterventionResponseType
+import com.google.gson.Gson
 import ma.glasnost.orika.MapperFacade
 import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestHeader
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import org.taktik.connector.business.domain.etarif.TarificationMediprimaConsultationResult
+import org.taktik.freehealth.middleware.dto.mycarenet.MycarenetError
+import org.taktik.freehealth.middleware.service.MediprimaService
 import java.time.Instant
 import java.time.LocalDate
+import java.time.LocalDateTime
 import java.time.ZoneId
 import java.time.format.DateTimeFormatter
 import java.util.UUID
@@ -20,7 +25,16 @@ internal val timezone: String = "Europe/Brussels"
 
 @RestController
 @RequestMapping("/mediprima")
-class MediprimaController(val mediprimaService: org.taktik.freehealth.middleware.service.MediprimaService, val mapper: MapperFacade) {
+class MediprimaController(
+    val mediprimaService: MediprimaService,
+    val mapper: MapperFacade
+) {
+    private val ConsultTarifErrors =
+        Gson().fromJson(
+            this.javaClass.getResourceAsStream("/be/errors/ConsultTarifErrors.json").reader(Charsets.UTF_8),
+            arrayOf<MycarenetError>().javaClass
+        ).associateBy({ it.uid }, { it })
+
     @PostMapping("/consultMediprima/{patientSsin}", produces = [MediaType.APPLICATION_JSON_UTF8_VALUE])
     fun consultMediprima(
         @RequestHeader(name = "X-FHC-keystoreId") keystoreId: UUID,
@@ -51,6 +65,42 @@ class MediprimaController(val mediprimaService: org.taktik.freehealth.middleware
             startDate = instantStartDate,
             endDate = instantEndDate,
             referenceDate = instantReferenceDate
+        )
+    }
+
+
+    @PostMapping("/consultTarificationMediprima/{patientSsin}", produces = [MediaType.APPLICATION_JSON_UTF8_VALUE])
+    fun consultMediprimaTarification(
+        @PathVariable patientSsin: String,
+        @RequestHeader(name = "X-FHC-tokenId") tokenId: UUID,
+        @RequestHeader(name = "X-FHC-keystoreId") keystoreId: UUID,
+        @RequestHeader(name = "X-FHC-passPhrase") passPhrase: String,
+        @RequestParam hcpFirstName: String,
+        @RequestParam hcpLastName: String,
+        @RequestParam hcpNihii: String,
+        @RequestParam hcpSsin: String,
+        @RequestParam date: Long,
+        @RequestParam(required = false) traineeSupervisorSsin: String?,
+        @RequestParam(required = false) traineeSupervisorNihii: String?,
+        @RequestParam(required = false) traineeSupervisorFirstName: String?,
+        @RequestParam(required = false) traineeSupervisorLastName: String?,
+        @RequestBody codes: List<String>
+    ): TarificationMediprimaConsultationResult? {
+        return mediprimaService.consultTarif(
+            keystoreId = keystoreId,
+            tokenId = tokenId,
+            hcpFirstName = hcpFirstName,
+            hcpLastName = hcpLastName,
+            hcpNihii = hcpNihii,
+            hcpSsin = hcpSsin,
+            passPhrase = passPhrase,
+            patientSsin = patientSsin,
+            consultationDate = date?.let { LocalDateTime.of((date / 10000).toInt(), ((date / 100).toInt() % 100), (date % 100).toInt(), 0, 0)} ?: LocalDateTime.now(),
+            traineeSupervisorSsin = traineeSupervisorSsin,
+            traineeSupervisorNihii = traineeSupervisorNihii,
+            traineeSupervisorFirstName = traineeSupervisorFirstName,
+            traineeSupervisorLastName = traineeSupervisorLastName,
+            codes = codes
         )
     }
 
