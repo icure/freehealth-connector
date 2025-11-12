17e5fe072c8a35adb74a9ef0f3d5176d2e37cbeb
Mennechet <maxime.mennechet@gmail.com>
2025-11-12 16:16:12 +0100

    Added async service for eAgreement

--- src/main/kotlin/org/taktik/freehealth/middleware/domain/eAgreement/EAgreementList.kt
+++ src/main/kotlin/org/taktik/freehealth/middleware/domain/eAgreement/EAgreementList.kt
@@ -4,9 +4,9 @@ import be.cin.types.v1.FaultType
 import org.taktik.freehealth.middleware.dto.mycarenet.MycarenetConversation
 import java.util.Date
 
-data class EAgreementDataList(
+data class EAgreementList(
     val mycarenetConversation: MycarenetConversation?,
-    val eAgreementMessageList: List<EAgreementMessageList>?,
+    val eAgreementMessageList: List<EAgreementMessage>?,
     val date: Date?,
     val genericErrors: List<FaultType>?
 )
--- src/main/kotlin/org/taktik/freehealth/middleware/domain/eAgreement/EAgreementResponse.kt
+++ src/main/kotlin/org/taktik/freehealth/middleware/domain/eAgreement/EAgreementResponse.kt
@@ -3,16 +3,14 @@ package org.taktik.freehealth.middleware.domain.eAgreement
 import be.cin.types.v1.FaultType
 import org.taktik.freehealth.middleware.domain.memberdata.MdaStatus
 import org.taktik.freehealth.middleware.dto.mycarenet.MycarenetError
-import org.taktik.icure.cin.saml.oasis.names.tc.saml._2_0.assertion.Assertion
 import javax.xml.datatype.XMLGregorianCalendar
 
-data class EAgreementBatchResponse(
+class EAgreementResponse(
     var status: MdaStatus? = null,
     var errors: List<FaultType>? = null,
     var myCarenetErrors: List<MycarenetError> = ArrayList(),
     var issuer: String? = null,
     var issueInstant: XMLGregorianCalendar? = null,
     var inResponseTo: String? = null,
-    var responseId: String? = null,
-    var assertions: List<Assertion> = ArrayList()
+    var responseId: String? = null
 )
