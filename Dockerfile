8c075a89ba1cb42cadd6c1a1e17273c71a48ac48
Lateur Jonas <jonas.lateur@cgm.com>
2026-01-20 14:27:36 +0100

    Prepare for publishing to internal docker repo

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,7 @@
 FROM adoptopenjdk/openjdk8:x86_64-alpine-jre8u462-b08
-
+LABEL maintainer="Compufit"
+LABEL version="0.0.1"
+LABEL description="FreeHealth Connector"
 
 ADD build/libs/freehealth-connector-0.0.0-dev.jar freehealth.jar
 ADD config.json /opt/freehealth/config.json
---
+++ build_and_push.bat
@@ -0,0 +1,50 @@
+@echo off
+
+REM echo Building Java/Kotlin Freehealth-connector...
+REM .\gradlew build
+
+set "IMAGE_NAME=freehealth-connector"
+set "TAG=develop"
+set "JFROG_URL="
+
+if "%JFROG_URL%"=="" (
+  echo ERROR: JFROG_URL is required.
+  echo.
+  echo Set it by passing it as the 3rd argument, e.g.:
+  echo   %~nx0 %IMAGE_NAME% %TAG% dockerrepo.domain.com
+  pause
+  exit /b 1
+)
+
+for /f "tokens=1 delims=/" %%A in ("%JFROG_URL%") do set "JFROG_REGISTRY=%%A"
+
+set "LOCAL_TAG=%IMAGE_NAME%:%TAG%"
+set "REMOTE_TAG=%JFROG_URL%/%IMAGE_NAME%:%TAG%"
+
+echo Building image: %LOCAL_TAG%
+docker buildx build -t %LOCAL_TAG% .
+if errorlevel 1 (
+  echo ERROR: docker build failed.
+  pause
+  exit /b 1
+)
+
+echo Tagging image: %REMOTE_TAG%
+docker tag %LOCAL_TAG% %REMOTE_TAG%
+if errorlevel 1 (
+  echo ERROR: docker tag failed.
+  pause
+  exit /b 1
+)
+
+echo Pushing image: %REMOTE_TAG%
+docker push %REMOTE_TAG%
+if errorlevel 1 (
+  echo ERROR: docker push failed.
+  pause
+  exit /b 1
+)
+
+echo Done.
+
+pause
